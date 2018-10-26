acff25dd2aff81cc03d1f2b48551682af82f9621
GuilhermaoAndrade <guilherme.souza366@etec.sp.gov.br>
2018-10-26 21:28:44 -0200

    Criação do arquivo Habibs.java

---
+++ Habibis/src/br/com/Habibs/Main.java
@@ -0,0 +1,12 @@
+
+package br.com.Habibs;
+
+public class Main 
+{
+    public static void main(String args[])
+    {
+   
+        System.out.println("Sistema criado com sucesso!");
+            
+    }
+}
